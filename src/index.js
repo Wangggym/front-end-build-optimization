e8913b3d75a170d68c0f905861fc29b41ed3229e
王毅敏 <wangyimin@jusda.int>
2019-12-26 09:07:55 +0800

    tree shaking

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import _ from 'lodash';
+// import _ from 'lodash';
+import { cube } from './math.js'
 function component() {
-    var element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], '');
+    var element = document.createElement('pre');
+    // element.innerHTML = _.join(['Hello', 'webpack'], '');
+    element.innerHTML = [
+        'Hello webpack!',
+        '5 cubed is equal to ' + cube(5)
+    ].join('\n\n');
     return element;
 }
 
